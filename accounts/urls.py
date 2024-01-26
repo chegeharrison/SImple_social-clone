10ed423e314922647bc652431ef88f1e70ec4518
harrry <chegeharrison10@gmail.com>
2024-01-26 22:56:04 +0300

    auth user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import  LoginView, LogoutView
 from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name='login'),
-    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
+    path("login/", LoginView.as_view(), name='login'),
+    path("logout/", LogoutView.as_view(), name='logout'),
     path("signup/", views.SignUp.as_view(), name='signup'),
 
 ]
