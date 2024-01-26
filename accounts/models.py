6d65929e5164d95cc0679e7724d7d62d7315ad1b
harrry <chegeharrison10@gmail.com>
2024-01-26 23:03:46 +0300

    removed auth useer

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,4 @@
 from django.db import models
 
 
-# Create your models here.
-# class User (auth.models.User, auth.models.PermissionsMixin):
 
-#     def __str__(self):
-#         return "@{}".format (self.username)
-    
