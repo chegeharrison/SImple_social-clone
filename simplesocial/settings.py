93408ff8f856b96b0c29d26301b3e0dd97657409
harrry <chegeharrison10@gmail.com>
2024-01-26 17:12:00 +0300

    fixed the home page fixed the delete button

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "bootstrap3",
+    'django_bootstrap5',
     "misaka",
+    'crispy_forms',
     # "django_markdown",
     "braces",
     "accounts",
@@ -125,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL ='test'
 LOGOUT_REDIRECT_URL = 'thanks'
@@ -135,3 +137,6 @@ LOGOUT_REDIRECT_URL = 'thanks'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
