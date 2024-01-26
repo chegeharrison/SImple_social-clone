307fe6031d78ae5a9202fd3e89696eea26a90ef5
harrry <chegeharrison10@gmail.com>
2024-01-26 21:20:35 +0300

    allowed host

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-wy&e_#c_ry5y3@w069nad=m@cpf@!f1cvgciclai#(=f^q71*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
