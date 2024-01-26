8d8a778d9d700b5232f6c4cf1d91ca06336cb41b
harrry <chegeharrison10@gmail.com>
2024-01-26 10:41:03 +0300

    fixed the create group

--- groups/admin.py
+++ groups/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .import models
 
 # Register your models here.
-class GroupForm(admin.ModelForm):
-    class Meta:
-        model = Group
-        exclude = ['description']
-
 class GroupMemberInline(admin.TabularInline):
     model = models.GroupMember
 
--- groups/models.py
+++ groups/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
-User = get_user_model()
 from django import template
-register = template.Library
+register = template.Library()
 from django.urls import reverse
 import misaka
 
+
+User = get_user_model()
 
 # Create your models here.
 
 class Group(models.Model):
     name = models.CharField(max_length=255, unique=True)
     slug = models.SlugField(allow_unicode=True, unique=True)
-    description = models.TextField(editable=False, default='', blank=True)
+    description = models.TextField( default='', blank=True)
     members = models.ManyToManyField(User, through='GroupMember')
 
     def __str__(self):
@@ -35,7 +36,7 @@ class GroupMember(models.Model):
     user = models.ForeignKey(User, related_name='user_groups', on_delete=models.CASCADE) 
 
     def __str__(self):
-        return self.username
+        return self.user.username
     
     class Meta:
         unique_together = ('group', 'user')
